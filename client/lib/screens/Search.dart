d2d2ba4f651058bd5d91fcca0b0f287595628499
Waleed ur Rehman <waleedrehman31@gmail.com>
2021-09-29 12:59:48 +0500

    :tada: Create Constant and edit some search :tada:

--- client/lib/screens/Search.dart
+++ client/lib/screens/Search.dart
@@ -10,8 +10,26 @@ class Search extends StatefulWidget {
 class _SearchState extends State<Search> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Text("You Can Search Here"),
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Container(
+          margin: EdgeInsets.all(10),
+          child: TextField(
+            decoration: const InputDecoration(
+              border: OutlineInputBorder(),
+              hintText: 'Enter a search term',
+              prefixIcon: Icon(Icons.search),
+              hintStyle: TextStyle(color: Colors.grey),
+              filled: true,
+              fillColor: Colors.white70,
+              focusedBorder: OutlineInputBorder(
+                borderRadius: BorderRadius.all(Radius.circular(10.0)),
+                borderSide: BorderSide(color: Colors.green, width: 2),
+              ),
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
