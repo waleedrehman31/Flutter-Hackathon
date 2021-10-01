1962c65c19ca235f74cec43d689a95d62f5bf585
Waleed ur Rehman <waleedrehman31@gmail.com>
2021-10-01 11:16:16 +0500

    :sparkles: some little UI tweeks in article details :sparkles:

--- client/lib/screens/ArticleDetail.dart
+++ client/lib/screens/ArticleDetail.dart
@@ -1,13 +1,65 @@
+import 'package:client/screens/authenticationScreen/Login.dart';
+import 'package:client/screens/authenticationScreen/Register.dart';
 import 'package:flutter/material.dart';
 import 'package:client/model/news_model.dart';
+import 'package:client/constant/constant.dart';
 
 class ArticleDetail extends StatelessWidget {
   final Article article;
+  bool isUser = false;
 
   ArticleDetail({this.article});
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        backgroundColor: primaryColor,
+        title: Text("News App"),
+        actions: <Widget>[
+          TextButton(
+            child: Text(
+              "Login",
+              style: TextStyle(
+                  fontSize: 16,
+                  fontWeight: FontWeight.bold,
+                  color: secondaryColor),
+            ),
+            onPressed: () => {},
+          ),
+          TextButton(
+            child: Text(
+              "Register",
+              style: TextStyle(
+                fontSize: 16,
+                fontWeight: FontWeight.bold,
+                color: secondaryColor,
+              ),
+            ),
+            onPressed: () => {},
+          ),
+          // CircleAvatar(
+          //   radius: 25,
+          //   backgroundImage: NetworkImage(
+          //       'https://cdn.pixabay.com/photo/2018/03/21/16/50/woman-3247382__340.jpg'),
+          // ),
+          // TextButton(
+          //   child: Text(
+          //     "Logout",
+          //     style: TextStyle(
+          //       fontWeight: FontWeight.bold,
+          //       color: secondaryColor,
+          //     ),
+          //   ),
+          //   onPressed: () => {
+          //     setState(
+          //       () {
+          //         currentScreen = Login();
+          //       },
+          //     )
+          //   },
+          // ),
+        ],
+      ),
       body: Padding(
         padding: const EdgeInsets.all(8.0),
         child: Column(
@@ -15,6 +67,7 @@ class ArticleDetail extends StatelessWidget {
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             Container(
+              margin: EdgeInsets.all(5.0),
               height: 200.0,
               width: double.infinity,
               decoration: BoxDecoration(
@@ -29,15 +82,16 @@ class ArticleDetail extends StatelessWidget {
               height: 8.0,
             ),
             Container(
-              padding: EdgeInsets.all(6.0),
+              padding:
+                  EdgeInsets.only(top: 6.0, bottom: 6.0, left: 8.0, right: 8.0),
               decoration: BoxDecoration(
-                color: Colors.red,
-                borderRadius: BorderRadius.circular(30.0),
+                color: primaryColor,
+                borderRadius: BorderRadius.circular(5.0),
               ),
               child: Text(
                 article.source.name,
                 style: TextStyle(
-                  color: Colors.white,
+                  color: secondaryColor,
                 ),
               ),
             ),
@@ -48,6 +102,12 @@ class ArticleDetail extends StatelessWidget {
               article.description,
               style: TextStyle(
                 fontWeight: FontWeight.bold,
+                fontSize: 16.0,
+              ),
+            ),
+            Text(
+              article.content,
+              style: TextStyle(
                 fontSize: 16.0,
               ),
             )
