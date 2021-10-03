13ecbd551d2ecbe5e0359bdc6c4c374bf0896954
Waleed ur Rehman <waleedrehman31@gmail.com>
2021-10-03 10:55:35 +0500

    :sparkles: Login Form Validation for User :sparkles:

--- client/lib/screens/authenticationScreen/Login.dart
+++ client/lib/screens/authenticationScreen/Login.dart
@@ -9,7 +9,8 @@ class Login extends StatefulWidget {
 }
 
 class _LoginState extends State<Login> {
-  bool _passwordVisible = true;
+  bool _passwordVisible = false;
+  final _formKey = GlobalKey<FormState>();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -39,88 +40,116 @@ class _LoginState extends State<Login> {
               margin: EdgeInsets.only(left: 10, right: 10),
               width: MediaQuery.of(context).size.width,
               height: MediaQuery.of(context).size.height / 2,
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: <Widget>[
-                  SizedBox(
-                    height: 10,
-                  ),
-                  TextFormField(
-                    decoration: const InputDecoration(
-                      border: OutlineInputBorder(),
-                      labelText: 'Email',
-                      icon: const Padding(
-                        padding: const EdgeInsets.only(top: 15.0),
-                        child: const Icon(
-                          Icons.email,
-                          color: primaryColor,
-                        ),
-                      ),
-                      hintStyle: TextStyle(color: Colors.grey),
-                      filled: true,
-                      fillColor: Colors.white70,
-                      focusedBorder: OutlineInputBorder(
-                        borderRadius: BorderRadius.all(Radius.circular(10.0)),
-                        borderSide: BorderSide(color: primaryColor, width: 2),
-                      ),
+              child: Form(
+                key: _formKey,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: <Widget>[
+                    SizedBox(
+                      height: 10,
                     ),
-                  ),
-                  SizedBox(
-                    height: 10,
-                  ),
-                  StatefulBuilder(builder: (_context, _setState) {
-                    // only following widget gets update when _setState is used
-                    return TextFormField(
-                      decoration: InputDecoration(
+                    TextFormField(
+                      autocorrect: false,
+                      textCapitalization: TextCapitalization.none,
+                      enableSuggestions: false,
+                      validator: (value) {
+                        if (value.isEmpty || !value.contains('@')) {
+                          return 'Please enter a valid email address.';
+                        }
+                        return null;
+                      },
+                      keyboardType: TextInputType.emailAddress,
+                      decoration: const InputDecoration(
+                        border: OutlineInputBorder(),
+                        labelText: 'Email',
+                        icon: const Padding(
+                          padding: const EdgeInsets.only(top: 15.0),
+                          child: const Icon(
+                            Icons.email,
+                            color: primaryColor,
+                          ),
+                        ),
+                        hintStyle: TextStyle(color: Colors.grey),
+                        filled: true,
+                        fillColor: Colors.white70,
                         focusedBorder: OutlineInputBorder(
                           borderRadius: BorderRadius.all(Radius.circular(10.0)),
                           borderSide: BorderSide(color: primaryColor, width: 2),
                         ),
-                        border: OutlineInputBorder(),
-                        suffixIcon: IconButton(
-                          icon: Icon(
-                            _passwordVisible
-                                ? Icons.visibility
-                                : Icons.visibility_off,
-                            color: primaryColor,
-                          ),
-                          onPressed: () {
-                            // use _setState that belong to StatefulBuilder
-                            _setState(() {
-                              _passwordVisible = !_passwordVisible;
-                            });
-                          },
-                        ),
-                        labelText: 'Password',
-                        icon: const Padding(
-                          padding: const EdgeInsets.only(top: 15.0),
-                          child: const Icon(
-                            Icons.lock,
-                            color: primaryColor,
-                          ),
-                        ),
-                      ),
-                      validator: (val) =>
-                          val.length < 6 ? 'Password too short.' : null,
-                      obscureText: true,
-                    );
-                  }),
-                  SizedBox(
-                    height: 10,
-                  ),
-                  ElevatedButton(
-                    onPressed: () => {},
-                    child: Text(
-                      "Login",
-                      style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                        fontSize: 18,
                       ),
                     ),
-                    style: ElevatedButton.styleFrom(primary: primaryColor),
-                  )
-                ],
+                    SizedBox(
+                      height: 10,
+                    ),
+                    StatefulBuilder(builder: (_context, _setState) {
+                      // only following widget gets update when _setState is used
+                      return TextFormField(
+                        key: ValueKey('password'),
+                        validator: (value) {
+                          if (value.isEmpty || value.length < 7) {
+                            return 'Password must be at least 7 characters long.';
+                          }
+                          return null;
+                        },
+                        obscureText: _passwordVisible ? false : true,
+                        decoration: InputDecoration(
+                          focusedBorder: OutlineInputBorder(
+                            borderRadius:
+                                BorderRadius.all(Radius.circular(10.0)),
+                            borderSide:
+                                BorderSide(color: primaryColor, width: 2),
+                          ),
+                          border: OutlineInputBorder(),
+                          suffixIcon: IconButton(
+                            icon: Icon(
+                              _passwordVisible
+                                  ? Icons.visibility
+                                  : Icons.visibility_off,
+                              color: primaryColor,
+                            ),
+                            onPressed: () {
+                              // use _setState that belong to StatefulBuilder
+                              _setState(() {
+                                _passwordVisible = !_passwordVisible;
+                              });
+                            },
+                          ),
+                          labelText: 'Password',
+                          icon: const Padding(
+                            padding: const EdgeInsets.only(top: 15.0),
+                            child: const Icon(
+                              Icons.lock,
+                              color: primaryColor,
+                            ),
+                          ),
+                        ),
+                      );
+                    }),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    ElevatedButton(
+                      onPressed: () {
+                        if (_formKey.currentState.validate()) {
+                          // If the form is valid, display a snackbar. In the real world,
+                          // you'd often call a server or save the information in a database.
+                          ScaffoldMessenger.of(context).showSnackBar(
+                            const SnackBar(content: Text('Processing Data')),
+                          );
+                        }
+                      },
+                      child: Text(
+                        "Login",
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 18,
+                        ),
+                      ),
+                      style: ElevatedButton.styleFrom(primary: primaryColor),
+                    )
+                  ],
+                ),
               ),
             ),
           ],
